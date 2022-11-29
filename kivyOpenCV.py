f3bd39d185530a9a70e9f113343685ad24789b1b
Inosuke0718 <8holeuk8@gmail.com>
2022-11-29 23:28:22 +0900

    kivyApp

--- kivyOpenCV.py
+++ kivyOpenCV.py
@@ -5,21 +5,19 @@ from kivy.uix.image import Image
 from kivy.graphics.texture import Texture
 from kivy.clock import Clock
 import cv2
+import datetime
+import calcDarts as cd
 
+# アプリの定義
 class MainApp(MDApp):
-
+    # ビルド時に呼ばれる(constructor)
     def build(self):
+        # レイアウトの生成
         layout = MDBoxLayout(orientation='vertical')
         self.image = Image()
         layout.add_widget(self.image)
-        # layout.add_widget(MDRaisedButton(
-        #     text='CLICK HERE',
-        #     pos_hint={'center_x': .5, 'center_y': .5},
-        #     size_hint=(None, None)
-        #     )
-        # )
         self.save_img_button = MDRaisedButton(
-            text='CLICK HERE',
+            text='CLICK HERE!',
             pos_hint={'center_x': .5, 'center_y': .5},
             size_hint=(None, None))
         self.save_img_button.bind(on_press=self.take_picture)
@@ -32,13 +30,25 @@ class MainApp(MDApp):
         ret, frame = self.capture.read()
         # Frame initilize
         self.image_frame = frame
+
+        # selfはメンバ変数的な感じだと思われ
+        #画像サイズ取得し、Centerを割り出す
+        height = self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        width = self.capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+        centerX = round(width/2)
+        centerY = round(height/2)
+
+        cd.calcDarts(centerX, centerY, ret, frame)
+
         buffer = cv2.flip(frame, 0).tostring()
         texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
         texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
+
         self.image.texture = texture
 
     def take_picture(self, *args):
-        image_name = "picture_at_2_02.jpg"
+        dt_now = datetime.datetime.now()
+        image_name = dt_now.strftime('%Y%m%d%H%M%S')+".jpg"
         cv2.imwrite(image_name, self.image_frame)
 
 if __name__ == '__main__':
